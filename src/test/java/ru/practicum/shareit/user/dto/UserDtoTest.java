4b8fce875e71efaa6c9a5fee460acda9a39e2563
boltaevt <boltaevt@gmail.com>
2023-06-14 00:09:57 +0500

    added test

---
+++ src/test/java/ru/practicum/shareit/user/dto/UserDtoTest.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.user.dto;
+
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDtoTest {
+    private final Validator validator;
+
+    public UserDtoTest() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void testValidUserDto() {
+        // Arrange
+        UserDto userDto = UserDto.builder()
+                .id(1L)
+                .name("John Doe")
+                .email("johndoe@example.com")
+                .build();
+
+        // Act
+        Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
+
+        // Assert
+        assertTrue(violations.isEmpty());
+    }
+}
